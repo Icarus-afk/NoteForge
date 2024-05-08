a653f28a6c7b762fd16f65d9357dd8e2359c0538
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-05-08 23:24:42 +0600

    Fix

--- backend/utils/initRedis.js
+++ backend/utils/initRedis.js
@@ -1,16 +1,16 @@
-import Redis from 'ioredis';
-import logger from './consoleLogger.js'
+// import Redis from 'ioredis';
+// import logger from './consoleLogger.js'
 
-const redisClient = new Redis({
-    host: 'localhost',
-    port: 6379, 
-  });
-  redisClient.on('connect', () => {
-   logger.info('Connected to Redis');
-  });
+// const redisClient = new Redis({
+//     host: 'localhost',
+//     port: 6379, 
+//   });
+//   redisClient.on('connect', () => {
+//    logger.info('Connected to Redis');
+//   });
   
-  redisClient.on('error', (err) => {
-    logger.error('Redis connection error:', err);
-  });
+//   redisClient.on('error', (err) => {
+//     logger.error('Redis connection error:', err);
+//   });
   
-  export default redisClient;
+//   export default redisClient;
