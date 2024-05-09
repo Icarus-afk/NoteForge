1b2bb6e2d460ebbfe6322e1affde719c573effde
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-05-09 17:30:46 +0600

    Cors fix

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:8000', 'http://127.0.0.1:5173'], 
+    origin: ['*'], 
     credentials: true
 }));
 
